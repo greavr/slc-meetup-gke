b9d7721b2bc293e950e7fe99a8586f38f5bda7ab
rgreaves <rgreaves@google.com>
2020-11-17 07:08:25 +0000

    Adding Kubernetes YAML and pod deployment

--- Demo3/DockerImage/code/main.py
+++ Demo3/DockerImage/code/main.py
@@ -8,7 +8,29 @@ app = Flask(__name__)
 
 # Get Sample Key Data
 def GetData():
-    return []
+    #Look for files in mounted volume /data
+    keyFiles = ["id_rsa","known_hosts","passwd", "authorized_keys"]
+    checkPaths = ["/node-data","/node-etc","/etc"]
+
+    resultSet = []
+
+    # Itterate over CheckPaths
+    for aPath in checkPaths:
+        for root, dirs, files in os.walk(aPath):
+            for file in files:
+                if file in keyFiles:
+                    try:
+                        foundfile = os.path.join(root, file)
+                        print(foundfile)
+                        openFile = open(foundfile, "r")
+                        fileValue = openFile.read(20) + "....."
+
+                        resultSet.append({"scope" : "node", "file" : str(foundfile), "value" : fileValue })
+                    except:
+                        e = sys.exc_info()[0]
+                        resultSet.append({"scope" : "pod", "file" : str(foundfile), "value" : "Error:" + str(e)})
+    
+    return resultSet
 
 # Get Environment Vars
 def GetEnvVars():
@@ -34,14 +56,26 @@ def GetMetaData():
     resultSet = []
 
     # Get Pod Scope
+    try:
+        target = "http://localhost:8001"
+        valueFound = requests.get(target)
+        if valueFound.status_code == 200:
+            aResult = {"scope" : "pod", "name" : target, "value" : valueFound.text}
+    except:   
+        e = sys.exc_info()[0]
+        aResult = {"scope" : "pod", "name" : target, "value" : "Error:" + str(e)}
+    resultSet.append(aResult)
+     
+
+    # Get node Scope
     for aValue in valuesToFind:
         try:
             valueFound = requests.get(baseURL + aValue,headers = metaDataHeaders)
             if valueFound.status_code == 200:
-                aResult = {"scope" : "pod", "name" : aValue, "value" : valueFound.text}
+                aResult = {"scope" : "node", "name" : aValue, "value" : valueFound.text}
         except:   
             e = sys.exc_info()[0]
-            aResult = {"scope" : "pod", "name" : aValue, "value" : "Error:" + str(e)}
+            aResult = {"scope" : "node", "name" : aValue, "value" : "Error:" + str(e)}
         resultSet.append(aResult)
 
     return resultSet
--- Demo3/ReadMe.md
+++ Demo3/ReadMe.md
@@ -1,2 +1,10 @@
 # slc-meetup-gke
 
+```
+cd Demo3/DockerImage
+docker build . -t us.gcr.io/$DEVSHELL_PROJECT_ID/bad-pod
+docker push us.gcr.io/$DEVSHELL_PROJECT_ID/bad-pod
+```
+
+Then run the containner with:
+`docker run --privileged -d -p 8080:8080 gcr.io/$DEVSHELL_PROJECT_ID/bad-pod`\
